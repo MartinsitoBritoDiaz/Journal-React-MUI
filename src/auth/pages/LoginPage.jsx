a55be284b83f4498d8343cc082856fb96a91e4f5
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-02-19 15:59:19 -0400

    feature: login with firebase

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -4,16 +4,21 @@ import { Button, Grid, TextField, Typography, Link } from '@mui/material'
 import { AuthLayout } from '../layout/AuthLayout'
 import { useForm } from '../../hooks'
 import { checkingAuthentication, startGoogleSignIn } from '../../store/auth/thunks'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useMemo } from 'react'
 
 
 export const LoginPage = () => {
+  const { status } = useSelector( state => state.auth );
   const dispatch = useDispatch();
 
-  const { email, password, onInputChange } = useForm({
+  const formData = {
     email: 'journalApp@gmail.com',
     password: '1234567'
-  });
+  }
+  const { email, password, onInputChange } = useForm( formData );
+
+  const isAuthenticating = useMemo( () => status === 'checking', [ status ]);
 
   const onSubmit = ( event ) => {
     event.preventDefault();
@@ -60,13 +65,21 @@ export const LoginPage = () => {
 
             <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='outlined' fullWidth type='submit'>
+                <Button 
+                  disabled={ isAuthenticating }
+                  variant='outlined' 
+                  fullWidth 
+                  type='submit'>
                   Login
                 </Button>
               </Grid>
 
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='outlined' fullWidth onClick={ onGoogleSignIn }>
+                <Button 
+                  disabled={ isAuthenticating }
+                  variant='outlined' 
+                  fullWidth 
+                  onClick={ onGoogleSignIn }>
                   <Google />
                   <Typography sx={{ ml: 1}}>Google</Typography>
                 </Button>
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,13 +2,37 @@ import { Link as RouterLink } from 'react-router-dom'
 import { AppRegistration, Google, Login } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography, Link } from '@mui/material'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from '../../hooks'
 
 export const RegisterPage = () => {
+  const { status } = useSelector( state => state.auth );
+  const dispatch = useDispatch();
+
+  const formData = {
+    displayName: 'Martinsito Brito',
+    email: 'journalApp@gmail.com',
+    password: '1234567'
+  }
+
+  const formValidations = {
+    email: [ (value) => value.includes('@'), 'Email must have the @ symbol'],
+    password: [ (value) => value.length >= 6, 'Password must have at least 6 characters'],
+    displayName: [ (value) => value.length >= 1, 'Name is required'],
+  }
+
+  const { displayName, displayNameValid, email, emailValid, password, passwordValid, onInputChange, formState, isFormValid } = useForm( formData );
+  
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log( formState );
+  }
+
   return (
     <AuthLayout 
       title='Register'
     >
-      <form>
+      <form onSubmit={ onSubmit }>
           <Grid container >
             <Grid item xs={ 12 } sx={{ mt: 2}}>
               <TextField 
@@ -16,7 +40,9 @@ export const RegisterPage = () => {
                 type="text"
                 placeholder="Martinsito Brito"
                 fullWidth
-
+                name='displayName'
+                value={ displayName }
+                onChange={ onInputChange }
               />
             </Grid>
 
@@ -26,7 +52,9 @@ export const RegisterPage = () => {
                 type="email"
                 placeholder="email@gmail.com"
                 fullWidth
-
+                name='email'
+                value={ email }
+                onChange={ onInputChange }
               />
             </Grid>
 
@@ -36,11 +64,15 @@ export const RegisterPage = () => {
                 type="password"
                 placeholder="Password"
                 fullWidth
+                name='password'
+                value={ password }
+                onChange={ onInputChange }
               />
             </Grid>
             
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
               <Button 
+                type='submit'
                 variant='contained' 
                 fullWidth
               >
